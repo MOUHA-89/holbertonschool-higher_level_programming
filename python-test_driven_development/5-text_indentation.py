203de02f9f3ff300ffa033fc7fa5857670d4e45e
MOUHA-89 <mouhamedkeita05@gmail.com>
2025-01-25 15:29:21 +0100

    5-text_indentation.py

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -2,20 +2,8 @@
 def text_indentation(text):
     if not isinstance(text, str):
         raise TypeError("text must be a string")
-
-    punctuation = ['.', '?', ':']
-    result = ""
-    skip_space = True
-
+    
     for char in text:
-        if skip_space and char == ' ':
-            continue
-
-        result += char
-        skip_space = False
-
-        if char in punctuation:
-            result += "\n\n"
-            skip_space = True
-
-    print(result.format())
+        print(char, end="")
+        if char in "?:.":
+            print("\n\n", end="")
