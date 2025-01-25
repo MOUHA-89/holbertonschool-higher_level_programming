0c1c56fef1ad0705b8f6b07cdb89607ae828eb89
MOUHA-89 <mouhamedkeita05@gmail.com>
2025-01-25 14:35:47 +0100

    4-print_square.py

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -2,10 +2,10 @@
 def print_square(size):
     """
     Prints a square with the character #.
-    
+
     Args:
     size (int): The size length of the square.
-    
+
     Raises:
     TypeError: If size is not an integer.
     ValueError: If size is less than 0.
@@ -14,6 +14,6 @@ def print_square(size):
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
-    
+
     for _ in range(size):
         print("#" * size)
