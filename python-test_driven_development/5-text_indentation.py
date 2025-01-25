b76d97f21c462619a0f312b18748d5583d80e996
MOUHA-89 <mouhamedkeita05@gmail.com>
2025-01-25 15:31:29 +0100

    5-text_indentation.py

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -2,7 +2,7 @@
 def text_indentation(text):
     if not isinstance(text, str):
         raise TypeError("text must be a string")
-    
+
     for char in text:
         print(char, end="")
         if char in "?:.":
