219e8918e455f402c6e8e300e6198cc09afe9c4e
MOUHA-89 <mouhamedkeita05@gmail.com>
2025-01-26 20:21:27 +0100

    9-max_integer.py

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-def max_integer(my_list=[]):
-    my_list.sort()
+def max_integer(my_list=None):
     if not my_list:
-        return none
-    else:
-        return my_list[-1]
+        return None
+    return max(my_list)
