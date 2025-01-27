78e3870536df950bdcfa57467ec7ac597953b53a
MOUHA-89 <mouhamedkeita05@gmail.com>
2025-01-27 15:06:27 +0100

    1-square.py

---
+++ python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Python program to demonstrate
+"""
+
+
+class Square:
+    """
+    nmake None
+    """
+    def __init__(self, size):
+        self.__size = size
+        
