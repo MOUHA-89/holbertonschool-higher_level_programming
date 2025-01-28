72a816cdb0b3d835d85479b623754eb4a64f7c2a
MOUHA-89 <mouhamedkeita05@gmail.com>
2025-01-28 16:32:13 +0100

    1-rectangle.py

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -82,3 +82,7 @@ class Rectangle:
 
                     if heigth < 0:
                         raise ValueError("height must be >= 0")
+                def __init__(self, width=0, height=0):
+                                self.width = width
+                                self.height = height
+
