1f6d0b1335d99d2f436ec2bad87eb849adaa43a2
MOUHA-89 <mouhamedkeita05@gmail.com>
2025-02-04 22:41:25 +0100

    4-inherits_from.py

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+python program
+"""
+
+
+def inherits_from(obj, a_class):
+    mo_type = type(obj)
+
+    if type(obj) is a_class:
+        return False
+    if a_class in mo_type.__mro__:
+        return True
