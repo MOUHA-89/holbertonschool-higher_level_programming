40d28f616a5ef812af25934389293fb387dd6202
MOUHA-89 <mouhamedkeita05@gmail.com>
2025-02-04 22:46:58 +0100

    4-inherits_from.py

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,16 +1,18 @@
-#!/usr/bin/python3
-"""
-python program
-"""
-
-
 def inherits_from(obj, a_class):
     """
-    this is method
+    Returns True if obj is an instance of a class that inherited (directly or indirectly)
+    from a_class, otherwise returns False.
+    
+    :param obj: The object to check
+    :param a_class: The class to compare against
+    :return: Boolean
     """
-    mo_type = type(obj)
-
-    if type(obj) is a_class:
+    # Get the type of the object
+    obj_type = type(obj)
+    
+    # Check if the object's type is exactly the same as a_class
+    if obj_type is a_class:
         return False
-    if a_class in mo_type.__mro__:
-        return True
+    
+    # Check if a_class is in the object type's method resolution order
+    return a_class in obj_type.__mro__
