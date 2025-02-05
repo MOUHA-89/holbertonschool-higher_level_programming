05535079c878d18852a74bcff8e4c083b7a23bd0
MOUHA-89 <mouhamedkeita05@gmail.com>
2025-02-05 02:46:01 +0100

    9-rectangle.py

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+This module defines a Rectangle class that inherits from BaseGeometry.
+"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """
+    A class representing a rectangle, inheriting from BaseGeometry.
+    """
+
+    def __init__(self, width, height):
+
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__(self):
+        return f"[rectangle] {self.__width}/{self.__height}"
