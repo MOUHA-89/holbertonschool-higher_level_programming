d03704eab280ad7af12d57e0b18866a5b5840f9d
MOUHA-89 <mouhamedkeita05@gmail.com>
2025-02-10 17:08:32 +0100

    1-write_file.py

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
+"""
+python program
+"""
+
+
 def write_file(filename="", text=""):
+    """
+    Reads a UTF-8 encoded text file and return its content to stdout.
+    """
 
     with open(filename, "w", encoding="UTF-8") as file:
         return (file.write(text))
