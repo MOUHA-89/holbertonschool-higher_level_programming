e952f9130085457e8de4f649ca203b149953838a
MOUHA-89 <mouhamedkeita05@gmail.com>
2025-02-10 17:04:45 +0100

    1-write_file.py

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def write_file(filename="", text=""):
 
-    with file(filename="", text="", encoding="UTF-8") as file:
-        file.write()
-        
+    with open(filename, "w", encoding="UTF-8") as file:
+        return (file.write(text))
