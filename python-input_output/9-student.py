fc4d60a33fac45fe42c6b5c2deca063bd47260d9
MOUHA-89 <mouhamedkeita05@gmail.com>
2025-02-10 23:11:54 +0100

    9-student.py

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
+"""
+python program
+"""
+
+
 class Student:
+    """
+    this is a class
+    """
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
