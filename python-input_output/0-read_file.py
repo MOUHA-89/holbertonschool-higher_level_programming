d612964456cd7314ce14f46f8d145af2be2f37ed
MOUHA-89 <mouhamedkeita05@gmail.com>
2025-02-10 16:27:13 +0100

    0-read_file.py

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -7,4 +7,4 @@ def read_file(filename=""):
     Reads a UTF-8 encoded text file and prints its content to stdout.
     """
     with open(filename, "r", encoding="utf-8") as file:
-        print(file.read())
+        print(file.read(), end="")
---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def write_file(filename="", text=""):
+
+    with file(filename="", text="", encoding="UTF-8") as file:
+        file.write()
+        
