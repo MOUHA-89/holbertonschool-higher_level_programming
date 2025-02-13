679e08393adf2ff071b4710267d325006e555439
MOUHA-89 <mouhamedkeita05@gmail.com>
2025-02-13 16:43:22 +0100

    task_01_pickle.py

---
+++ python-serialization/task_01_pickle.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+python program
+"""
+import pickle
+
+class CustomObject:
+    """
+    this is a class
+    """
+    def display(self, name, age, is_student):
+        self.name = name
+        self.age = age
+        self.is_student = is_student
+
+    def serialize(self, filename):
+        """
+        method to print out the object’s attributes
+        """
+        with open(filename, "w", encosding= "UTF-8") as file:
+            pickle.dump(self, file)
+        
+        with open(filename, "r", encosding="UTF-8") as file:
+
+            return pickle.load(self)
+        
+    @classmethod 
+    def deserialize(cls, filename):
+        """
+        This class method will take a filename as its parameter
+        """
+
+        with open(filename, "r", encoding="UTF-8") as file:
+            
+            return pickle.load(file)
