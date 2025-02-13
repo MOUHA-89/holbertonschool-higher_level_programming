153cf56af1f4bad39ca66c292b9468ef3ee64fde
MOUHA-89 <mouhamedkeita05@gmail.com>
2025-02-13 15:52:56 +0100

    task_00_basic_serialization.py

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -4,22 +4,31 @@ python program
 """
 import json
 
-
 def serialize_and_save_to_file(data, filename):
     """
-     serialize a Python dictionary to a JSON file
+    Serialize a Python dictionary to JSON and save it to a file
+    
+    Args:
+        data: A Python Dictionary with data
+        filename: The filename of the output JSON file
     """
-    with open(filename, 'w', encoding= "UTF-8") as file:
+    try:
+        with open(filename, 'w', encoding= "UTF-8") as file:
             json.dump(data, file)
-    print(f"Data successfully serialized and saved to {filename}.")
+        print(f"Data successfully serialized and saved to {filename}.")
+    except Exception as e:
+        print(f"An error occurred while saving data to {filename}: {e}")
 
 
 def load_and_deserialize(filename):
     """
-    Loads and deserializes a JSON file
+    Save a python dictionary to JSON and serealise it to a file
     """
-    with open(filename, 'r', encoding= "UTF-8") as file:
+    try:
+        with open(filename, 'r', encoding= "UTF-8") as file:
             data = json.load(file)
-    print(f"Data successfully loaded and deserialized from {filename}.")
-    
-    
+        print(f"Data successfully loaded and deserialized from {filename}.")
+        return data
+    except Exception as e:
+        print(f"An error occurred while loading data from {filename}: {e}")
+        return None
