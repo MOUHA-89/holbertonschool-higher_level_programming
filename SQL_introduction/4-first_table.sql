d7076e7543df1a124f9d96e976ffd976c7d935f3
MOUHA-89 <mouhamedkeita05@gmail.com>
2025-03-03 23:06:06 +0100

    4-first_table.sql

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,3 +1,2 @@
 --  creates a table called first_table in the current database in your MySQL server
-CREATE TABLE IF NOT EXISTS first_table(id INT AUTO_INCREMENTE PRIMARY KEY,
-name  VARCHAR(256) NOT NULL)
+CREATE TABLE IF NOT EXISTS first_table(id INT AUTO_INCREMENTE PRIMARY KEY, name  VARCHAR(256) NOT NULL)
