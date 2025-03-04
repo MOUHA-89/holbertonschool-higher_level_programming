ea9ed91f82fc8d651b979a39a7efdc47ca2c7f2e
MOUHA-89 <mouhamedkeita05@gmail.com>
2025-03-04 21:59:39 +0100

    1-create_user.sql

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,3 @@
 -- creates the MySQL server user user_0d_1.
-CREATE USER user_0d_1@localhost
-IDENTIFIED BY 'Secure1Pass!';
-SHOW GRANTS FOR user_0d_1@localhost;
+CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
