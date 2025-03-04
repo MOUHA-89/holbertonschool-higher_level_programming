7edd0e789fa02d6aaaff3457ff7175e053e01f0f
MOUHA-89 <mouhamedkeita05@gmail.com>
2025-03-04 21:27:30 +0100

    0-privileges.sql

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,2 +1,3 @@
 -- ists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server (in localhost)
-SELECT user_0d_1, user_0d_2 host FROM mysql.users;
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'loaclhost';
