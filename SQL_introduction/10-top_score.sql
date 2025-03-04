9418c170cf93a7d8052de586365292f503331abd
MOUHA-89 <mouhamedkeita05@gmail.com>
2025-03-04 01:01:35 +0100

    10-top_score.sql

--- SQL_introduction/10-top_score.sql
+++ SQL_introduction/10-top_score.sql
@@ -1,4 +1,6 @@
---  lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server
-SELECT name, score 
-FROM second_table 
+-- lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server
+SELECT name, score
+-- container  
+FROM second_table
+-- order 
 ORDER BY score DESC;
