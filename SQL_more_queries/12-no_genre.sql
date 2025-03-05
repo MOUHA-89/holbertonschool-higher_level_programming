f9e420e46accf148fe5aed6cf2905b26bd24616b
MOUHA-89 <mouhamedkeita05@gmail.com>
2025-03-05 21:00:40 +0100

    12-no_genre.sql

--- SQL_more_queries/12-no_genre.sql
+++ SQL_more_queries/12-no_genre.sql
@@ -2,4 +2,4 @@
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows AS s
 LEFT JOIN tv_show_genres AS g ON s.id = g.show_id
-ORDER BY s.title, g.genre_id ASC;
+ORDER BY s.title ASC, g.genre_id ASC;
