3dea7b983e949fd8cb25978e986e783b001df84f
MOUHA-89 <mouhamedkeita05@gmail.com>
2025-03-05 14:40:35 +0100

    8-cities_of_california_subquery.sql

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,10 +1,5 @@
--- Select the id and name of cities
-SELECT cities.id, cities.name
+-- lists all the cities of California that can be found in the database hbtn_0d_usa
+SELECT cities.name
 FROM cities
--- Join the 'cities' table with the 'states' table using the foreign key 'state_id'
--- and the primary key 'id' of the 'states' table
 JOIN states ON cities.state_id = states.id
--- Filter the results to keep only cities located in California
-WHERE states.name = 'California'
--- Sort the results by city id in ascending order
-ORDER BY cities.id ASC;
+WHERE states.name = 'California';
