05a86079255dd75a376619bac500084d81e346a2
MOUHA-89 <mouhamedkeita05@gmail.com>
2025-03-05 20:59:00 +0100

    12-no_genre.sql

--- SQL_more_queries/12-no_genre.sql
+++ SQL_more_queries/12-no_genre.sql
@@ -1,5 +1,5 @@
 -- lists all shows contained in hbtn_0d_tvshows without a genre linked
 SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_show AS s
+FROM tv_shows AS s
 LEFT JOIN tv_show_genres AS g ON s.id = g.show_id
 ORDER BY s.title, g.genre_id ASC;
