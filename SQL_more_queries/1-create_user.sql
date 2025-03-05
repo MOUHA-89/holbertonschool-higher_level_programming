df5453e02aed162bce98137d492e30c10f06569c
MOUHA-89 <mouhamedkeita05@gmail.com>
2025-03-05 11:05:56 +0100

    1-creat_user.sql

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,8 @@
--- creates the MySQL server user user_0d_1.
+-- Créer l'utilisateur user_0d_1 s'il n'existe pas
 CREATE USER IF NOT EXISTS 'user_0d_1'@'%' IDENTIFIED BY 'user_0d_1_pwd';
+
+-- Accorder tous les privilèges à user_0d_1 sur l'ensemble du serveur MySQL
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'%' WITH GRANT OPTION;
+
+-- Appliquer les modifications de privilèges immédiatement
 FLUSH PRIVILEGES;
