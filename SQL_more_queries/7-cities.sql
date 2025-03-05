a44dd9982995a276584b315c7a863edb2ed121bf
MOUHA-89 <mouhamedkeita05@gmail.com>
2025-03-05 13:48:32 +0100

    7-cities.sql

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -3,7 +3,8 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 -- Use newly database
 USE hbtn_0d_usa;
 -- create table
-CREATE TABLE IF NOT EXISTS cities(id INT UNIQUE AUTO_INCREMENT PRIMARY KEY; -- auto generated unique end not null
+CREATE TABLE IF NOT EXISTS cities(
+id INT UNIQUE AUTO_INCREMENT PRIMARY KEY; -- auto generated unique end not null
 state_id INT NOT NULL, -- not null 
 name VARCHAR(256) NOT NULL, -- name not null
 FOREIGN KEY (state_id) REFERENCES States(id) -- FOREIGN KEY that references to id of the states table
