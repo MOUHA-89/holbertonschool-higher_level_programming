1f3c008632021a68dafd1c6aa60f0c350f4acf3d
MOUHA-89 <mouhamedkeita05@gmail.com>
2025-03-05 10:48:28 +0100

    1-create_user.sql

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 -- creates the MySQL server user user_0d_1.
-CREATE USER 'user_0d_1'@'localhost'IDENTIFIED BY 'user_0d_1pwd';
+CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
