b54ee79d47227a04da750a977fa54cd488dbcacf
MOUHA-89 <mouhamedkeita05@gmail.com>
2025-03-05 13:09:47 +0100

    6-states.sql

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,4 +1,6 @@
 -- create database
 CREATE DATABASE hbtn_0d_usa;
+-- Use database
+USE hbtn_0d_usa;
 -- create table
 CREATE TABLE IF NOT EXISTS states(id INT UNIQUE,AUTO_GENERATED, PRIMARY KEY, name VARCHAR(256) NOT NULL);
