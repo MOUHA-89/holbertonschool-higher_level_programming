f792250a93ea83546d2f9302867380d0cdb3e5e7
MOUHA-89 <mouhamedkeita05@gmail.com>
2025-03-05 14:31:07 +0100

    1-create_user.sql

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,8 +1,6 @@
 -- Check if the user already exists; if not, create the user
-CREATE USER IF NOT EXISTS 'user_0d_1'@'%' IDENTIFIED BY 'user_0d_1_pwd';
-
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 -- Grant all privileges on the MySQL server
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'%' WITH GRANT OPTION;
-
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
 -- Apply the changes
 FLUSH PRIVILEGES;
