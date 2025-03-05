14b991d9924fcde0d29465bca2217f0a01ce7e4f
MOUHA-89 <mouhamedkeita05@gmail.com>
2025-03-05 13:02:33 +0100

    6-states.sql

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,4 +1,4 @@
 -- create database
 CREATE DATABASE hbtn_0d_usa;
 -- create table
-CREATE TABLE states(id INT UNIQUE, name VARCHAR(256) NOT NULL);
+CREATE TABLE IF NOT EXISTS states(id INT UNIQUE, name VARCHAR(256) NOT NULL);
