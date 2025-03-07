6c8406959646030b2547e2c96ed7f86c3f6bc105
MOUHA-89 <mouhamedkeita05@gmail.com>
2025-03-07 14:04:52 +0100

    1-filter_states.py

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,38 +1,28 @@
 #!/usr/bin/python3
-import sys
 import MySQLdb
+import sys
 
-if __name__ == "__main__":
-    # Get arguments from the command line
-    mysql_user = sys.argv[1]
-    mysql_password = sys.argv[2]
-    database_name = sys.argv[3]
+if __name__ == '__main__':
 
-    # Connect to the MySQL database
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=mysql_user,
-        passwd=mysql_password,
-        db=database_name
-    )
+    username = sys.arg[1]
+    password = sys.arg[2]
+    database = sys.arg[3]
 
-    # Create a cursor object to interact with the database
-    cursor = db.cursor()
+db = MySQLdb.connect(host="localhost",
+                     user="your_username",
+                     passwd="your_password",
+                     db="hbtn_0e_0_usa")
 
-    # Execute the query to get states starting
-    # with 'N' and order them by state.id
-    cursor.execute(
-        "SELECT name FROM states WHERE name LIKE 'N%' ORDER BY id ASC"
-    )
+cursor = db.cursor()
 
-    # Fetch all the results
-    states = cursor.fetchall()
+query = "SELECT name FROM states WHERE name LIKE 'N%' ORDER BY id ASC;"
 
-    # Print the states
-    for state in states:
-        print(state[0])
+cursor.execute(query)
 
-    # Close the cursor and connection
-    cursor.close()
-    db.close()
+states = cursor.fetchall()
+
+for state in states:
+    print(state[0])
+
+cursor.close()
+db.close()
