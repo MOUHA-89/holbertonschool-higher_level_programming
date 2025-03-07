936dcb04bf4cf64bb585a7ca8bc131e8fdd16646
MOUHA-89 <mouhamedkeita05@gmail.com>
2025-03-07 16:35:20 +0100

    3-my_safe_filter_states.py

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,20 +1,32 @@
 #!/usr/bin/python3
-"""takes in an argument and displays all values
-in the states table of hbtn_0e_0_usa
-where name matches the argument
-and is safe from SQL injections"""
+""" Prevent SQL Injection """
 
-if __name__ == '__main__':
+from sys import argv
+import MySQLdb
 
-    import MySQLdb
-    import sys
-
-    db = MySQLdb.connect(host='localhost', port=3306,
-                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-
+if __name__ == "__main__":
+    username = argv[1]
+    password = argv[2]
+    db_name = argv[3]
+    state_name = argv[4]
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=username,
+                         passwd=password,
+                         db=db_name)
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name=%s\
-                ORDER BY states.id ASC", (sys.argv[4],))
+
+    query = """
+    SELECT states.id, name FROM states WHERE name = %s
+    COLLATE latin1_general_cs
+    ORDER BY states.id ASC;
+    """
+
+    cur.execute(query, (state_name, ))
+
     rows = cur.fetchall()
     for row in rows:
         print(row)
+
+    cur.close()
+    db.close()
---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,6 @@
+#!/user/bin/python3
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    
