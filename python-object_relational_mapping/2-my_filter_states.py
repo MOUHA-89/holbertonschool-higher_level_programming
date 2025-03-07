a8e8788384d611b0b503462f5c074ef91ff0c1d2
MOUHA-89 <mouhamedkeita05@gmail.com>
2025-03-07 15:02:22 +0100

    2-my_filter_states.py

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+""""
+"takes in an argument and displays all values in the states
+table of hbtn_0e_0_usa where name matches the argument."
+"""
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    username = sys.arg[1]
+    password = sys.arg[2]
+    database = sys.arg[3]
+    state = sys.arg[4]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        user=username,
+        passwd=password,
+        db=database,
+        st=state,
+    )
+
+    cursor = db.cursor()
+
+    query = "SELECT *FROM states WHERE name ORDER BY id"
+
+    cursor.execute(query, (state,))
+
+    rows = cursor.fetchall
+
+    for row in rows:
+        print(row)
+
+        cursor.close()
+        db.close()
