bb1a326a71e05ede038c599d48f6d4bc904f00da
MOUHA-89 <mouhamedkeita05@gmail.com>
2025-03-07 19:22:51 +0100

    5-filter_cities.py

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
+"""
+ takes in the name of a state as an argument and lists
+   all cities of that state, using the database hbtn_0e_4_usa
+ """
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    
+
     if len(sys.argv) != 5:
-        print("Usage: ./script.py <mysql_username> <mysql_password> <database_name> <state_name>")
+        print("Usage: ./script.py <mysql_username> <mysql_password> "
+              "<database_name> <state_name>")
         sys.exit(1)
 
     mysql_username = sys.argv[1]
@@ -13,7 +18,8 @@ if __name__ == "__main__":
     database_name = sys.argv[3]
     state_name = sys.argv[4]
 
-    db = MySQLdb.connect(host="localhost", port=3306, user=mysql_username, passwd=mysql_password, db=database_name)
+    db = MySQLdb.connect(host="localhost", port=3306, user=mysql_username,
+                         passwd=mysql_password, db=database_name)
     cursor = db.cursor()
 
     query = """
@@ -23,7 +29,7 @@ if __name__ == "__main__":
         WHERE states.name = %s
         ORDER BY cities.id ASC
     """
-    
+
     cursor.execute(query, (state_name,))
 
     cities = cursor.fetchall()
