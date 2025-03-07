7a39e0f8253a2d65f7142e9e70082f6dbc05fea8
MOUHA-89 <mouhamedkeita05@gmail.com>
2025-03-07 16:52:10 +0100

    4-cities_by_state.py

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -1,18 +1,30 @@
 #!/usr/bin/python3
-"""Script to list all cities from database `hbtn_0e_4_usa`"""
+"""
+This script takes in an argument and
+displays all values in the states
+where `name` matches the argument
+from the database `hbtn_0e_0_usa`.
+This time the script is safe from
+MySQL injections!
+"""
+
+import MySQLdb as db
+from sys import argv
 
 if __name__ == "__main__":
-    import MySQLdb
-    import sys
+    """
+    Access to the database and get the states
+    from the database.
+    """
+    db_connect = db.connect(host="localhost", port=3306,
+                            user=argv[1], passwd=argv[2], db=argv[3])
 
-    db = MySQLdb.connect(user=sys.argv[1],
-                         passwd=sys.argv[2],
-                         database=sys.argv[3])
-    cur = db.cursor()
-    cur.execute("SELECT cities.`id`, cities.`name`, states.`name` FROM cities "
-                "JOIN states ON states.`id` = cities.`state_id` "
-                "ORDER BY cities.`id` ASC")
-    for row in cur.fetchall():
+    db_cursor = db_connect.cursor()
+    db_cursor.execute(
+        "SELECT * FROM states WHERE name LIKE \
+                    BINARY %(name)s ORDER BY states.id ASC", {'name': argv[4]})
+
+    rows_selected = db_cursor.fetchall()
+
+    for row in rows_selected:
         print(row)
-    cur.close()
-    db.close()
